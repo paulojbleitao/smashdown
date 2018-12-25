db08aab642228ad0af1c84525d054439ccee77cf
Paulo Leitão <paulo.leitao@ccc.ufcg.edu.br>
2018-12-25 19:57:46 -0300

    Updates

---
+++ precache-manifest.30255cbf0030e9686b62b7c843cea11c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69febe134165141bcbf8",
+    "url": "/smashdown/static/js/runtime~main.69febe13.js"
+  },
+  {
+    "revision": "e6caf9a148393cdb2e39",
+    "url": "/smashdown/static/js/main.e6caf9a1.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/smashdown/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "e6caf9a148393cdb2e39",
+    "url": "/smashdown/static/css/main.d1982456.chunk.css"
+  },
+  {
+    "revision": "7ae7fa8884ba0a26559a1a1746b9d7c2",
+    "url": "/smashdown/index.html"
+  }
+];
